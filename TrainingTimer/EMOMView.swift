fe0959e0140190e8df897db931e15661f099fa23
Kerry Nitz <kerry.nitz@gmail.com>
2026-05-31 15:53:09 +1200

    Fix dynamic sizing of text for different form sizes (#6)
    Fix dynamic sizing of text for different form sizes and remove unused
    views

--- TrainingTimer/EMOMView.swift
+++ TrainingTimer/EMOMView.swift
@@ -11,60 +11,77 @@ struct EMOMView: View {
     @StateObject private var vm = EMOMViewModel()
     let totalSets: Int
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let width: Double = 200
+    private let widthFactor: Double = 0.05
     
     var body: some View {
-        VStack {
-            Text("Lead In: \(vm.leadIn)")
-                .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
-                .animation(.default, value: vm.isLeadingIn)
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.clear)
-            Text("Sets to go: \(vm.setsToGo)")
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-                .onAppear { vm.setData(totalSets)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("Lead In: \(vm.leadIn)")
+                        .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
+                        .animation(.default, value: vm.isLeadingIn)
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.clear)
+                    Spacer(minLength: geo.size.width * 0.1)
                 }
-            Text("\(vm.time)")
-                .font(.headline)
-                .alert("Finished!", isPresented: $vm.showingAlert) {
-                    Button("Continue", role: .cancel) {
-                        vm.reset()
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("Sets to go: \(vm.setsToGo)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                        .onAppear {
+                            vm.setData(totalSets)
+                        }
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("\(vm.time)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .alert("Finished!", isPresented: $vm.showingAlert) {
+                            Button("Continue", role: .cancel) {
+                                vm.reset()
+                            }
+                        }
+                        .padding()
+                        .frame(width: geo.size.width * 0.8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                    HStack(spacing:50) {
+                    Button("Start") {
+                        vm.startSets(sets: vm.sets)
                     }
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .disabled(vm.isActive)
+                    
+                    Button("Reset", action: vm.reset)
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .tint(.red)
                 }
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-            HStack(spacing:50) {
-                Button("Start") {
-                    vm.startSets(sets: vm.sets)
-                }
-                .disabled(vm.isActive)
-                
-                Button("Reset", action: vm.reset)
-                    .tint(.red)
+                Spacer()
             }
-            .frame(width: width)
+            .onReceive(timer) { _ in
+                vm.updateLeadIn()
+                vm.updateCountdown()
+            }
         }
-        .onReceive(timer) { _ in
-            vm.updateLeadIn()
-            vm.updateCountdown()
-        }
-        
     }
 }
 
--- TrainingTimer/TUTView.swift
+++ TrainingTimer/TUTView.swift
@@ -13,54 +13,71 @@ struct TUTView: View {
     let activeTime: Int
     let restTime: Int
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let width: Double = 200
+    private let widthFactor: Double = 0.05
     
     var body: some View {
-        VStack {
-            Text("Lead In: \(vm.leadIn)")
-                .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
-                .animation(.default, value: vm.isLeadingIn)
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.clear)
-            Text("Sets to go: \(vm.setsToGo)")
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-                .onAppear { vm.setData(totalSets, activeTime: activeTime, restTime: restTime)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("Lead In: \(vm.leadIn)")
+                        .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
+                        .animation(.default, value: vm.isLeadingIn)
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.clear)
+                    Spacer(minLength: geo.size.width * 0.1)
                 }
-            Text("\(vm.time)")
-                .font(.headline)
-                .alert("Finished!", isPresented: $vm.showingAlert) {
-                    Button("Continue", role: .cancel) {
-                        vm.reset()
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("Sets to go: \(vm.setsToGo)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                        .onAppear { vm.setData(totalSets, activeTime: activeTime, restTime: restTime)
+                        }
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("\(vm.time)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .alert("Finished!", isPresented: $vm.showingAlert) {
+                            Button("Continue", role: .cancel) {
+                                vm.reset()
+                            }
+                        }
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                HStack(spacing:50) {
+                    Button("Start") {
+                        vm.startSets(sets: vm.sets)
                     }
+                        .disabled(vm.isActive)
+                        .font(.system(size: geo.size.width * widthFactor))
+                    
+                    Button("Reset", action: vm.reset)
+                        .tint(.red)
+                        .font(.system(size: geo.size.width * widthFactor))
                 }
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-            HStack(spacing:50) {
-                Button("Start") {
-                    vm.startSets(sets: vm.sets)
-                }
-                .disabled(vm.isActive)
-                
-                Button("Reset", action: vm.reset)
-                    .tint(.red)
+                Spacer()
             }
-            .frame(width: width)
         }
         .onReceive(timer) { _ in
             vm.updateLeadIn()
