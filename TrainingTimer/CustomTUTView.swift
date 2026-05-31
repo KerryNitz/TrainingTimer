52851d4a27aad69b9737a1c87c84702d7324e1bc
KerryNitz <kerry.nitz@gmail.com>
2026-05-31 15:51:43 +1200

    Fix dynamic sizing of text for different form sizes

--- TrainingTimer/CustomTUTView.swift
+++ TrainingTimer/CustomTUTView.swift
@@ -15,60 +15,75 @@ struct CustomTUTView: View {
     @AppStorage("timeResting") private var timeResting = "30"
     
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let width: Double = 200
+    private let widthFactor: Double = 0.05
     
     var body: some View {
-        VStack {
-            Text("Lead In: \(vm.leadIn)")
-                .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
-                .animation(.default, value: vm.isLeadingIn)
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.clear)
-            Text("Sets to go: \(vm.setsToGo)")
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-                .onAppear { vm.setData((Int($totalRounds.wrappedValue) ?? 0) * (Int($totalExercises.wrappedValue) ?? 0), activeTime: Int($timeUnderTension.wrappedValue) ?? 0, restTime: Int($timeResting.wrappedValue) ?? 0)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                HStack {
+                    Text("Lead In: \(vm.leadIn)")
+                        .opacity(vm.isLeadingIn ? 1 : 0) // Hides text layout-neutrally if not leading in
+                        .animation(.default, value: vm.isLeadingIn)
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.clear)
+                    Spacer(minLength: geo.size.width * 0.1)
                 }
-            Text("\(vm.time)")
-                .font(.headline)
-                .alert("Finished!", isPresented: $vm.showingAlert) {
-                    Button("Continue", role: .cancel) {
-                        vm.reset()
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("Sets to go: \(vm.setsToGo)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                        .onAppear { vm.setData((Int($totalRounds.wrappedValue) ?? 0) * (Int($totalExercises.wrappedValue) ?? 0), activeTime: Int($timeUnderTension.wrappedValue) ?? 0, restTime: Int($timeResting.wrappedValue) ?? 0)
+                        }
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                HStack {
+                    Spacer(minLength: geo.size.width * 0.1)
+                    Text("\(vm.time)")
+                        .font(.system(size: geo.size.width * widthFactor))
+                        .alert("Finished!", isPresented: $vm.showingAlert) {
+                            Button("Continue", role: .cancel) {
+                                vm.reset()
+                            }
+                        }
+                        .padding()
+                        .frame(width: geo.size.width * 0.8, alignment: .center)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.gray, lineWidth: 4)
+                        )
+                    Spacer(minLength: geo.size.width * 0.1)
+                }
+                HStack(spacing:50) {
+                    Button("Start") {
+                        vm.startSets(sets: vm.sets)
                     }
+                    .disabled(vm.isActive)
+                    .font(.system(size: geo.size.width * widthFactor))
+                    
+                    Button("Reset", action: vm.reset)
+                        .tint(.red)
+                        .font(.system(size: geo.size.width * widthFactor))
                 }
-                .padding()
-                .frame(width: width)
-                .background(.thinMaterial)
-                .cornerRadius(20)
-                .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.gray, lineWidth: 4)
-                    )
-            HStack(spacing:50) {
-                Button("Start") {
-                    vm.startSets(sets: vm.sets)
-                }
-                .disabled(vm.isActive)
-                
-                Button("Reset", action: vm.reset)
-                    .tint(.red)
+                Spacer()
             }
-            .frame(width: width)
+            .onReceive(timer) { _ in
+                vm.updateLeadIn()
+                vm.updateCountdown()
+            }
         }
-        .onReceive(timer) { _ in
-            vm.updateLeadIn()
-            vm.updateCountdown()
-        }
-        
     }
 }
 
--- TrainingTimer/SettingsView.swift
+++ TrainingTimer/SettingsView.swift
@@ -13,83 +13,89 @@ struct SettingsView: View {
     @AppStorage("timeUnderTension") private var timeUnderTension = "30"
     @AppStorage("timeResting") private var timeResting = "30"
     
-    private let width: Double = 150
-    private let inputWidth: Double = 75
+    private let widthFactor: Double = 0.025
     
     var body: some View {
-        VStack {
-            Text("Settings")
-                .font(.headline)
-                .padding()
-                .frame(width: width)
-                .background(.clear)
-            HStack {
-                Text("Number of Rounds:")
-                    .font(.headline)
+        GeometryReader { geo in
+            VStack {
+                Text("Settings")
+                    .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                    .bold()
                     .padding()
-                    .frame(width: width, alignment: Alignment.trailing)
+                    .frame(maxWidth: .infinity, alignment: .center)
                     .background(.clear)
-                TextField("Number of Rounds:", text: $totalRounds)
-                    .font(.headline)
-                    .padding()
-                    .frame(width: inputWidth, alignment: Alignment.trailing)
-                    .background(.thinMaterial)
-                    .cornerRadius(20)
-                    .overlay(
+                HStack {
+                    Text("Number of Rounds:")
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.7, alignment: Alignment.trailing)
+                        .background(.clear)
+                    TextField("Number of Rounds:", text: $totalRounds)
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.2, alignment: Alignment.trailing)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color.gray, lineWidth: 4)
                         )
-            }
-            HStack {
-                Text("Number of Exercises:")
-                    .font(.headline)
-                    .padding()
-                    .frame(width: width, alignment: Alignment.trailing)
-                    .background(.clear)
-                TextField("Number of Exercises:", text: $totalExercises)
-                    .font(.headline)
-                    .padding()
-                    .frame(width: inputWidth, alignment: Alignment.trailing)
-                    .background(.thinMaterial)
-                    .cornerRadius(20)
-                    .overlay(
+                    Spacer(minLength: geo.size.width * 0.05)
+                }
+                HStack {
+                    Text("Number of Exercises:")
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.7, alignment: Alignment.trailing)
+                        .background(.clear)
+                    TextField("Number of Exercises:", text: $totalExercises)
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.2, alignment: Alignment.trailing)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color.gray, lineWidth: 4)
                         )
-            }
-            HStack {
-                Text("Time under tension (s):")
-                    .font(.headline)
-                    .padding()
-                    .frame(width: width, alignment: Alignment.trailing)
-                    .background(.clear)
-                TextField("Time under tension (s):", text: $timeUnderTension)
-                    .font(.headline)
-                    .padding()
-                    .frame(width: inputWidth, alignment: Alignment.trailing)
-                    .background(.thinMaterial)
-                    .cornerRadius(20)
-                    .overlay(
+                    Spacer(minLength: geo.size.width * 0.05)
+                }
+                HStack {
+                    Text("Time under tension (s):")
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.7, alignment: Alignment.trailing)
+                        .background(.clear)
+                    TextField("Time under tension (s):", text: $timeUnderTension)
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.2, alignment: Alignment.trailing)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color.gray, lineWidth: 4)
                         )
-            }
-            HStack {
-                Text("Time resting (s):")
-                    .font(.headline)
-                    .padding()
-                    .frame(width: width, alignment: Alignment.trailing)
-                    .background(.clear)
-                TextField("Time resting (s):", text: $timeResting)
-                    .font(.headline)
-                    .padding()
-                    .frame(width: inputWidth, alignment: Alignment.trailing)
-                    .background(.thinMaterial)
-                    .cornerRadius(20)
-                    .overlay(
+                    Spacer(minLength: geo.size.width * 0.05)
+                }
+                HStack {
+                    Text("Time resting (s):")
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.7, alignment: Alignment.trailing)
+                        .background(.clear)
+                    TextField("Time resting (s):", text: $timeResting)
+                        .font(.system(size: geo.size.width > geo.size.height ? geo.size.width * widthFactor : geo.size.width * widthFactor * 2))
+                        .padding()
+                        .frame(width: geo.size.width * 0.2, alignment: Alignment.trailing)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                        .overlay(
                             RoundedRectangle(cornerRadius: 20)
                                 .stroke(Color.gray, lineWidth: 4)
                         )
+                    Spacer(minLength: geo.size.width * 0.05)
+                }
             }
         }
     }
